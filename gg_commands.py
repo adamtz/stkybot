fb9280eaa15c9e91dc735226ee2694bb1713885c
adam98971@gmail.com <adam98971@gmail.com>
2019-07-10 14:07:51 -0400

    redid spam protection

--- gg_commands.py
+++ gg_commands.py
@@ -22,36 +22,35 @@ def sendText_mention(text, mention_id, mention_name):
 
 def parseMessage(message):
 	#check if someone is abusing the bot, if they have an entry in the cache then they hit too many times
-	if cache.has(message['name']):
-		status = cache.get(message['name'])
-		if status != "banned":
-			sendText_mention("@" + message['name']+ " ,you are using the bot too much, no bot for you for 30 seconds", message['user_id'], message['name'])
-			#delete from cache first so we can update the timeout via set
-			print ("banning: " + message['name'])
-			cache.delete(message['name'])
-			cache.set(message['name'], "banned", timeout = 30)
-		else:
-			print ("ignoring commands from: " + message['name'])
-			return None
+	status = cache.get(message['name'])
+	if status != "banned":
+		#add all users to this cache so they cant overwhelm the bot
+		cache.set(message['name'], "used", timeout = 5)
+		if (message['text'] == '!help'):
+			to_send = 'List of Commands:\n!mfl:get mfl commands\n!random:get a random number'
+			sendText(to_send)
+		elif (message['text'] == '!mfl'):
+			to_send = 'MFL Stuff: work in progress'
+			sendText(to_send)
+		elif (message['text'] == '!drew'):
+			to_send = 'Drew, Start the draft please'
+			sendText(to_send)
+		elif (message['text'] == '!random'):
+			to_send = str(random.randint(1,100))
+			print (to_send)
+			sendText(to_send)
+		elif (message['text'] == '!woat'):
+			mention_id = message['user_id']
+			mention_name = message['name']
+			#build message to send with the user to mention
+			to_send = 'you are the worst @' + mention_name
+			sendText_mention(to_send, mention_id, mention_name)
+	elif status == "used":
+		print ("banning: " + message['name'])
+		#delete from cache first so we can update the timeout via set
+		cache.delete(message['name'])
+		cache.set(message['name'], "banned", timeout = 30)
+		sendText_mention("@" + message['name']+ " ,you are using the bot too much, no bot for you for 30 seconds", message['user_id'], message['name'])
 	else:
-	#add all users to this cache so they cant overwhelm the bot
-		cache.set(message['name'], "", timeout = 5)
-	if (message['text'] == '!help'):
-		to_send = 'List of Commands:\n!mfl:get mfl commands\n!random:get a random number'
-		sendText(to_send)
-	elif (message['text'] == '!mfl'):
-		to_send = 'MFL Stuff: work in progress'
-		sendText(to_send)
-	elif (message['text'] == '!drew'):
-		to_send = 'Drew, Start the draft please'
-		sendText(to_send)
-	elif (message['text'] == '!random'):
-		to_send = str(random.randint(1,100))
-		print (to_send)
-		sendText(to_send)
-	elif (message['text'] == '!woat'):
-		mention_id = message['user_id']
-		mention_name = message['name']
-		#build message to send with the user to mention
-		to_send = 'you are the worst @' + mention_name
-		sendText_mention(to_send, mention_id, mention_name)
+		print ("ignoring commands from: " + message['name'])
+		return None
